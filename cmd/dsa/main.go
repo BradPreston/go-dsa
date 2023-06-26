5fbad4a8da83e99ee4be1c0d7af91f6c35743dca
bradpreston <bap5393@gmail.com>
2023-06-26 15:53:19 -0400

    Use queue in main.go

--- cmd/dsa/main.go
+++ cmd/dsa/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+
+	dataStructures "github.com/BradPreston/go-dsa/data-structures"
+)
+
+func main() {
+    q := dataStructures.Queue{}
+    q.Enqueue("first")
+    q.Enqueue("second")
+    q.Enqueue("third")
+    q.Print()
+
+    removed, err := q.Dequeue()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(removed)
+    fmt.Println("after remove\n=================")
+
+    q.Print()
+}
