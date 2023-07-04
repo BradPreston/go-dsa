f62a0963d7be7da4a8c349190748043233758483
bradpreston <bap5393@gmail.com>
2023-07-03 20:31:59 -0400

    Add test for push/pop and method for pop to doublyLinkedList

---
+++ doublyLinkedList/doublyLinkedList.go
@@ -0,0 +1,83 @@
+package doublyLinkedList
+
+import (
+	"errors"
+	"fmt"
+)
+
+type node struct {
+	Value any
+	next  *node
+	prev  *node
+}
+
+type DoublyLinkedList struct {
+	Head   *node
+	Tail   *node
+	Length int
+}
+
+// var indexOutOfBoundsErr = "index must be greater than zero and less than the length of the list."
+var emptyListErr = "list already has length of zero."
+
+// Print loops over each node in the list and prints it to the console.
+func (d *DoublyLinkedList) Print() {
+	current := d.Head
+
+	for {
+		if current.next == nil {
+			fmt.Printf("current: %v, prev: %v, next: nil\n", current.Value, current.prev.Value)
+			break
+		}
+
+		if current.prev == nil {
+			fmt.Printf("current: %v, prev: nil, next: nil\n", current.Value)
+		} else {
+			fmt.Printf("current: %v, prev: %v, next: %v\n", current.Value, current.prev.Value, current.next.Value)
+		}
+
+		current = current.next
+	}
+}
+
+// Push adds a node to the end of the list.
+func (d *DoublyLinkedList) Push(val any) {
+	newNode := node{
+		Value: val,
+		next:  nil,
+		prev:  nil,
+	}
+
+	if d.Length == 0 {
+		d.Head = &newNode
+		d.Tail = &newNode
+	} else {
+		d.Tail.next = &newNode
+		newNode.prev = d.Tail
+		d.Tail = &newNode
+	}
+
+	d.Length += 1
+}
+
+// Pop removes the last node from the list and returns it's value.
+func (d *DoublyLinkedList) Pop() (*node, error) {
+	if d.Length == 0 {
+		return nil, errors.New(emptyListErr)
+	}
+
+	currentTail := d.Tail
+
+	if d.Length == 1 {
+		d.Head = nil
+		d.Tail = nil
+	} else {
+		d.Tail = currentTail.prev
+		d.Tail.next = nil
+		currentTail.prev = nil
+	}
+
+	d.Length -= 1
+
+	return currentTail, nil
+}
---
+++ doublyLinkedList/doublyLinkedList_test.go
@@ -0,0 +1,78 @@
+package doublyLinkedList
+
+import "testing"
+
+var incorrectLengthError = "%s: incorrect list length. got %d, but wanted %d"
+var lengthOfZeroError = "list already has length of zero."
+var incorrectNodeValueError = "%s: node contains incorrect value. got %s, but wanted %s"
+var incorrectErrError = "%s: incorrect error. got %s, but wanted %s"
+var unexpectedErrError = "%s: got an error, but didn't expect one"
+
+func Test_Push(t *testing.T) {
+  tests := []struct {
+    name            string
+    values          []string
+    lengthAfterPush int
+  }{
+    {"adds one node to end of list", []string{"one"}, 1},
+    {"adds three nodes to end of list", []string{"one", "two", "three"}, 3},
+  }
+
+  for _, test := range tests {
+    l := DoublyLinkedList{}
+
+    for _, value := range test.values {
+      l.Push(value)
+    }
+
+    if l.Length != test.lengthAfterPush {
+      t.Errorf(incorrectLengthError, test.name, l.Length, test.lengthAfterPush)
+    }
+  }
+}
+
+func Test_Pop(t *testing.T) {
+	tests := []struct {
+		name           string
+		values         []string
+		lengthAfterPop int
+		expectedErr    bool
+	}{
+		{"removes the last item from a list with length of 1", []string{"one"}, 0, false},
+		{"removes the last item from a list with a length of 3", []string{"one", "two", "three"}, 2, false},
+		{"throws an error if trying to pop from empty list", []string{}, 0, true},
+		{"popped node has a value of 'one'", []string{"one"}, 0, false},
+	}
+
+	for _, test := range tests {
+		l := DoublyLinkedList{}
+
+		for _, value := range test.values {
+			l.Push(value)
+		}
+
+		got, err := l.Pop()
+		if err != nil {
+			if test.expectedErr {
+				if err.Error() != lengthOfZeroError {
+					t.Errorf(incorrectErrError, test.name, err.Error(), lengthOfZeroError)
+				}
+			} else {
+				t.Errorf(unexpectedErrError, test.name)
+			}
+
+			continue
+		}
+
+		if l.Length != test.lengthAfterPop {
+			t.Errorf(incorrectLengthError, test.name, l.Length, test.lengthAfterPop)
+		}
+
+		want := test.values[len(test.values)-1]
+
+		if got.Value != want {
+			t.Errorf(incorrectNodeValueError, test.name, got.Value, want)
+		}
+	}
+
+}
